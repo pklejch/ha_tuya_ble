4fc03e5b1fb3efd56f804daee6a8fff82cab1ac1
Klejch Petr <petr.klejch@lmc.eu>
2024-10-17 20:47:59 +0200

    fix: add fixes for new tuya API

--- custom_components/tuya_ble/const.py
+++ custom_components/tuya_ble/const.py
@@ -20,7 +20,7 @@ CONF_PRODUCT_MODEL: Final = "product_model"
 CONF_PRODUCT_NAME: Final = "product_name"
 
 TUYA_API_DEVICES_URL: Final = "/v1.0/users/%s/devices"
-TUYA_API_FACTORY_INFO_URL: Final = "/v1.0/iot-03/devices/factory-infos?device_ids=%s"
+TUYA_API_FACTORY_INFO_URL: Final = "/v1.0/devices/factory-infos?device_ids=%s"
 TUYA_FACTORY_INFO_MAC: Final = "mac"
 
 BATTERY_STATE_LOW: Final = "low"
